4578095a25c71055224fcdb25dbb3c80c1ffea84
lucius323 <sungsin323@gmail.com>
2021-06-12 17:56:03 +0900

    first commit

---
+++ src/test/java/com/example/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.example.datajpa.repository;
+
+import com.example.datajpa.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+
+        Member findMember =memberRepository.findById(savedMember.getId()).get();
+
+        assertThat(savedMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+
+    }
+}
